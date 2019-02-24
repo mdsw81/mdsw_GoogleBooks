ec71398fcd00a38b271d6f9e0f8ba096d5866510
Mark Sperle-Weiler <40145728+mdsw81@users.noreply.github.com>
2019-02-23 20:21:42 -0600

    Add files via upload

---
+++ src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+var query = "https://www.googleapis.com/books/v1/volumes?q=";
+
+export default {
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(param) {
+    return axios.get(query + param.title);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+};
